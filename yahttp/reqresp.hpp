c8596fbeac3e8ebfdacc11047f02b2381a521784
Aki Tuomi <cmouse@desteem.org>
2013-10-21 14:19:58 +0300

    AsyncRequestLoader

--- yahttp/reqresp.hpp
+++ yahttp/reqresp.hpp
@@ -7,12 +7,17 @@
 #include "utility.hpp"
 #include "exception.hpp"
 
+#ifndef YAHTTP_MAX_REQUEST_SIZE 
+#define YAHTTP_MAX_REQUEST_SIZE 2097152
+#endif
+
 namespace YaHTTP {
   typedef std::map<std::string,std::string> strstr_map_t;
 
   class Response;
   class Request;
   class AsyncResponseLoader;
+  class AsyncRequestLoader;
 
   class Request {
   public:
@@ -20,6 +25,24 @@ namespace YaHTTP {
      Request(const Response &resp);
      Request(const Request &req);
      ~Request();
+
+     void build(const std::string &method, const std::string &url, const std::string &params) {
+        this->method = method;
+        std::transform(this->method.begin(), this->method.end(), this->method.begin(), ::toupper);
+        this->url.parse(url);
+        this->headers["host"] = this->url.host;
+        this->headers["connection"] = "close";
+        this->headers["user-agent"] = "yahttp 0.1";
+        this->headers["accept"] = "*/*";
+        if (params.empty() == false) {
+           this->headers["content-type"] = "application/x-www-form-urlencoded; charset=utf-8";
+           this->headers["content-length"] = params.size();
+           this->body = params;
+        } else {
+           this->body = "";
+        }
+     };
+
      void load(std::istream &is);
      void write(std::ostream &os) const;
 
@@ -30,7 +53,10 @@ namespace YaHTTP {
      URL url;
      std::string method;
      std::string body;
+
+     friend std::istream& operator>>(std::istream& os, const Request &req);
      friend std::ostream& operator<<(std::ostream& os, const Request &req);
+     friend class AsyncRequestLoader;
   };
 
   class Response {
@@ -53,6 +79,7 @@ namespace YaHTTP {
      std::string body;
 
      friend std::istream& operator>>(std::istream& is, Response &resp);
+     friend std::ostream& operator<<(std::ostream& os, const Response &resp);
      friend class AsyncResponseLoader;
   };
 
@@ -74,6 +101,26 @@ namespace YaHTTP {
     std::ostringstream bodybuf;
   };
 
+  class AsyncRequestLoader {
+  public:
+    AsyncRequestLoader(Request *request) {
+      state = 0;
+      chunked = false;
+      chunk_size = 0;
+      maxbody = 0;
+      this->request = request;
+    };
+    bool feed(const std::string &somedata);
+  private:
+    Request *request;
+    int state;
+    std::string buffer;
+    bool chunked;
+    int chunk_size;
+    std::ostringstream bodybuf;
+    long maxbody;
+  };
+
   Request::Request() {};
   Request::Request(const Response &resp) {
         method = resp.method;
@@ -89,14 +136,6 @@ namespace YaHTTP {
         body = req.body;
      };
   Request::~Request() {};
-
-  void Request::load(std::istream &is) {  };
-  void Request::write(std::ostream &os) const { os << "FIXME"; };
-
-  std::ostream& operator<<(std::ostream& os, const Request &req) {
-    req.write(os);
-    return os;
-  };
 
   Response::Response() {};
   Response::Response(const Request &req) {
@@ -131,7 +170,7 @@ namespace YaHTTP {
   };
 
   void Response::write(std::ostream &os) const { 
-     os << "HTTP/1.1 " << status << " "; 
+     os << "HTTP/1.1 " << status << " ";
      if (statusText.empty()) 
        os << Utility::status2text(status);
      else
@@ -147,8 +186,33 @@ namespace YaHTTP {
      os << std::endl;
      os << body;
   };
+ 
+  void Request::load(std::istream &is) {
+     AsyncRequestLoader arl(this);
+     while(is.good()) {
+        char buf[1024];
+        is.read(buf, 1024);
+        if (is.gcount()) { // did we actually read anything
+           is.clear();
+           if (arl.feed(std::string(buf, is.gcount())) == true) return; // completed
+        }
+     }
+  };
 
-  std::ostream& operator<<(std::ostream& os, Response &resp) {
+  void Request::write(std::ostream &os) const {
+     os << method << " " << url.path << " HTTP/1.1" << std::endl;
+     strstr_map_t::const_iterator iter = headers.begin();
+     while(iter != headers.end()) {
+        os << Utility::camelizeHeader(iter->first) << ": " << iter->second << std::endl;
+        iter++;
+     }
+     os << std::endl;
+     if (body.size()>0) {
+        os << body;
+     }
+  };
+
+  std::ostream& operator<<(std::ostream& os, const Response &resp) {
      resp.write(os);
      return os;
   };
@@ -156,6 +220,114 @@ namespace YaHTTP {
   std::istream& operator>>(std::istream& is, Response &resp) {
      resp.load(is);
      return is;
+  };
+
+  std::ostream& operator<<(std::ostream& os, const Request &req) {
+     req.write(os);
+     return os;
+  };
+
+  std::istream& operator>>(std::istream& is, Request &req) {
+     req.load(is);
+     return is;
+  };
+
+  bool AsyncRequestLoader::feed(const std::string &somedata) {
+       size_t pos;
+
+       buffer.append(somedata);
+       while(state < 2) {
+          // need to find newline in buffer
+          if ((pos = buffer.find('\n')) == std::string::npos) return false;
+          std::string line(buffer.begin(), buffer.begin()+pos); // exclude newline
+          buffer.erase(buffer.begin(), buffer.begin()+pos+1); // remove line from buffer including newline
+          if (state == 0) { // startup line
+             std::string ver;
+             std::string tmpurl;
+             std::istringstream iss(line);
+             iss >> request->method >> tmpurl >> ver;
+             if (ver != "HTTP/1.1")
+                throw ParseError("Not a HTTP 1.1 request");
+             // uppercase the request method
+             std::transform(request->method.begin(), request->method.end(), request->method.begin(), ::toupper);
+             request->url.parse(tmpurl);
+             state = 1;
+          } else if (state == 1) {
+             std::string key,value;
+             size_t pos;
+             if (line.empty()) {
+               chunked = (request->headers.find("transfer-encoding") != request->headers.end() && request->headers["transfer-encoding"] == "chunked");
+               // validate certain headers
+               if (request->headers.find("host") == request->headers.end()) 
+                  throw ParseError("Missing Host header");
+               else 
+                  request->url.host = request->headers["host"];
+               // parse url
+               
+               state = 2;
+               break;
+             }
+             // split headers
+             if ((pos = line.find_first_of(": ")) == std::string::npos)
+               throw ParseError("Malformed hader");
+             key = line.substr(0, pos);
+             value = line.substr(pos+2);
+             std::transform(key.begin(), key.end(), key.begin(), ::tolower);
+             request->headers[key] = value;
+          }
+       }
+
+       if (request->method != "POST") 
+          return true;
+       
+       // do we have content-length? 
+       if (!chunked) {
+          if (request->headers.find("content-length") != request->headers.end()) {
+             std::istringstream maxbodyS(request->headers["content-length"]);
+             maxbodyS >> maxbody;
+          }
+          if (maxbody < 1) return true; // guess there isn't anything left.
+          if (maxbody > YAHTTP_MAX_REQUEST_SIZE) 
+                  throw ParseError("Request size exceeded");
+       }
+
+       if (buffer.size() == 0) return false;
+
+       while(buffer.size() > 0 && bodybuf.str().size() < static_cast<size_t>(maxbody)) {
+         char buf[1024] = {0};
+
+         if (chunked) {
+           if (chunk_size == 0) {
+             // read chunk length
+             if ((pos = buffer.find('\n')) == std::string::npos) return false;
+             if (pos > 1023)
+               throw ParseError("Impossible chunk_size");
+             buffer.copy(buf, pos);
+             buf[pos]=0; // just in case...
+             buffer.erase(buffer.begin(), buffer.begin()+pos+1); // remove line from buffer
+             sscanf(buf, "%x", &chunk_size);
+             if (!chunk_size) break; // last chunk
+           } else {
+             if (buffer.size() < static_cast<size_t>(chunk_size+1)) return false; // expect newline
+             if (buffer.at(chunk_size) != '\n') return false; // there should be newline.
+             buffer.copy(buf, chunk_size);
+             buffer.erase(buffer.begin(), buffer.begin()+chunk_size+1);
+             bodybuf << buf;
+             chunk_size = 0;
+             if (buffer.size() == 0) return false; // just in case
+           }
+         } else {
+             bodybuf << buffer;
+             buffer = "";
+         }
+       }
+
+       if (chunk_size!=0) return false; // need more data
+
+       bodybuf.flush();
+       request->body = bodybuf.str();
+       bodybuf.str("");
+       return true;
   };
 
   bool AsyncResponseLoader::feed(const std::string &somedata) {
@@ -228,5 +400,5 @@ namespace YaHTTP {
        response->body = bodybuf.str();
        bodybuf.str("");
        return true;
-    };
+  };
 };
--- yahttp/url.hpp
+++ yahttp/url.hpp
@@ -3,78 +3,109 @@
 #include <sstream>
 #include <string>
 
+#ifndef YAHTTP_MAX_URL_LENGTH
+#define YAHTTP_MAX_URL_LENGTH 2048
+#endif 
+
 namespace YaHTTP {
   class URL {
-  public:
-      URL() {};
-      URL(const std::string& url) {
-          parse(url);
-      }
-      URL(const char *url) { 
-          parse(std::string(url));
-      }
-      bool parse(const std::string& url) {
-          // get protocol first
-          size_t pos1,pos2;
-          if ( (pos1 = url.find_first_of(":")) == std::string::npos ) return false;
-          protocol = url.substr(0,pos1);
+   private: 
+      bool parseSchema(const std::string& url, size_t &pos) {
+          size_t pos1;
+          if (pos >= url.size()) return false; // no data
+          if ( (pos1 = url.find_first_of(":",pos)) == std::string::npos ) return false; // schema is mandatory
+          protocol = url.substr(pos, pos1-pos);
           if (protocol == "http") port = 80;
           if (protocol == "https") port = 443;
+          pos = pos1+1; // after :
+          if (url.compare(pos, 2, "//") == 0) {
+             pathless = false; // if this is true we put rest into parameters
+             pos += 2;
+          }
+          return true;
+      }
 
-          if (pos1+1 == url.length()) return true; // just protocol
-  
-          // get the part after proto, before host
-          if (url.compare(pos1, 3, "://") == 0) {
-             if ( (pos2 = url.find_first_of("/", pos1+3)) == std::string::npos ) return false;
-             host = url.substr(pos1+3,pos2-pos1-3);
+      bool parseHost(const std::string& url, size_t &pos) {
+          size_t pos1;
+          if (pos >= url.size()) return true; // no data
+          if ( (pos1 = url.find_first_of("/", pos)) == std::string::npos ) {
+             host = url.substr(pos);
+             path = "/";
+             pos = url.size();
           } else {
-             if ( (pos2 = url.find_first_of("/", pos1)) == std::string::npos ) {
-                host = url.substr(pos1+1);
-                return true;
-             }
-             host = url.substr(pos1+1,pos2);
+             host = url.substr(pos, pos1-pos);
+             pos = pos1;
           }
-  
-          path = url.substr(pos2);
-  
-          // now we have host, see if it contains username or port
-          if ( (pos1 = host.find_first_of("@")) != std::string::npos ) {
-             // yep.
-             username = host.substr(0,pos1);
-             host = host.substr(pos1+1);
-             // does username contain password?
-             if ( (pos1 = username.find_first_of(":")) != std::string::npos ) {
-               password = username.substr(pos1+1);
-               username = username.substr(0,pos1);
-             }
+          return true;
+      }
+
+      bool parseUserPass(const std::string& url, size_t &pos) {
+          size_t pos1,pos2;
+          if (pos >= url.size()) return true; // no data
+
+          if ( (pos1 = url.find_first_of("@",pos)) == std::string::npos ) return true; // no userinfo
+          pos2 = url.find_first_of(":",pos);
+
+          if (pos2 != std::string::npos) { // comes with password
+             username = url.substr(pos, pos2 - pos);
+             password = url.substr(pos2+1, pos1 - pos2 - 1);
+             password = Utility::decodeURL(password);
+          } else {
+             username = url.substr(pos+1, pos1 - pos);
           }
-          // see if there is port
-          if ( (pos1 = host.find_first_of(":")) != std::string::npos ) {
-             port = atoi(host.substr(pos1+1).c_str());
-             host = host.substr(0,pos1);
-          }
-          // see if path has parameters
-          if ( (pos1 = path.find_first_of("?")) != std::string::npos ) {
-             parameters = path.substr(pos1+1);
-             path = path.substr(0,pos1);
-          }
-          // see if parameters has anchors
-          if ( (pos1 = parameters.find_first_of("#")) != std::string::npos) {
-             anchor = parameters.substr(pos1+1);
-             parameters = parameters.substr(0,pos1);
-          }
-  
-          // decode some parts
+          pos = pos1+1;
           username = Utility::decodeURL(username);
-          password = Utility::decodeURL(password);
+          return true;
+      };
+
+      bool parsePath(const std::string& url, size_t &pos) {
+          size_t pos1;
+          if (pos >= url.size()) return true; // no data
+          if (url[pos] != '/') return false; // not an url
+          if ( (pos1 = url.find_first_of("?", pos)) == std::string::npos ) {
+             path = url;
+             pos = url.size();
+          } else {
+             path = url.substr(pos, pos1-pos);
+             pos = pos1;
+          }
           path = Utility::decodeURL(path);
           return true;
       }
-  
+
+      bool parseParameters(const std::string& url, size_t &pos) {
+          size_t pos1;
+          if (pos >= url.size()) return true; // no data
+          if (url[pos] == '#') return true; // anchor starts here
+          if (url[pos] != '?') return false; // not a parameter
+          if ( (pos1 = url.find_first_of("#", pos)) == std::string::npos ) {
+             parameters = url.substr(pos+1);;
+             pos = url.size();
+          } else {
+             parameters = url.substr(pos+1, pos1-pos-1);
+             pos = pos1;
+          }
+          return true;
+      }
+
+      bool parseAnchor(const std::string& url, size_t &pos) {
+          if (pos >= url.size()) return true; // no data
+          if (url[pos] != '#') return false; // not anchor
+          anchor = url.substr(pos+1);
+          return true;
+      }
+
+  public:
       std::string to_string() const {
           std::string tmp;
           std::ostringstream oss;
-          oss << protocol << "://";
+            
+          if (protocol.empty() == false) {
+             oss << protocol << ":";
+             if (host.empty() == false) {
+               oss << "//";
+             }
+          }
 
           if (username.empty() == false) {
            if (password.empty() == false)
@@ -82,14 +113,19 @@ namespace YaHTTP {
            else
              oss << Utility::encodeURL(username) << "@";
           }
-
-          oss << host;
+          if (host.empty() == false)
+             oss << host;
           if (!(protocol == "http" && port == 80) &&
-              !(protocol == "https" && port == 443)) 
+              !(protocol == "https" && port == 443) &&
+              port > 0) 
             oss << ":" << port;
+
           oss << Utility::encodeURL(path, true);
-          if (parameters.empty() == false)
-             oss << "?" << parameters;
+          if (parameters.empty() == false) {
+             if (!pathless) 
+                oss << "?";
+             oss << parameters;
+          }
           if (anchor.empty() == false)
              oss << "#" << anchor;
           return oss.str();
@@ -103,6 +139,38 @@ namespace YaHTTP {
       std::string path;
       std::string parameters;
       std::string anchor;
+      bool pathless;
+
+      URL() { protocol = ""; host = ""; port = 0; username = ""; password = ""; path = ""; parameters = ""; anchor =""; pathless = true; };
+      URL(const std::string& url) {
+          parse(url);
+      };
+
+      URL(const char *url) {
+          parse(std::string(url));
+      };
+
+      bool parse(const std::string& url) {
+          // setup
+          protocol = ""; host = ""; port = 0; 
+          username = ""; password = ""; path = ""; 
+          parameters = ""; anchor =""; pathless = true;
+
+          if (url.size() > YAHTTP_MAX_URL_LENGTH) return false;
+          size_t pos = 0;
+          if (*(url.begin()) != '/') { // full url?
+             if (parseSchema(url, pos) == false) return false;
+             if (pathless) {
+                parameters = url.substr(pos);
+                return true;
+             }
+             if (parseUserPass(url, pos) == false) return false;
+             if (parseHost(url, pos) == false) return false;
+          }
+          if (parsePath(url, pos) == false) return false;
+          if (parseParameters(url, pos) == false) return false;
+          return parseAnchor(url, pos);
+      };
 
       friend std::ostream & operator<<(std::ostream& os, const URL& url) {
          os<<url.to_string();

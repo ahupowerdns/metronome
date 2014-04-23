014adcbe46ace8dba9f31716f7ef4a287931be29
Aki Tuomi <cmouse@desteem.org>
2014-04-23 09:49:44 +0300

    Simple tests for request

--- tests/test-request-simple.cpp
+++ tests/test-request-simple.cpp
@@ -12,7 +12,24 @@ BOOST_AUTO_TEST_SUITE(test_request_simple)
 
 BOOST_AUTO_TEST_CASE(test_request_parse)
 {
-   BOOST_CHECK(true);
+  std::ifstream ifs("request-001.txt");
+  YaHTTP::Request req;
+  YaHTTP::Response resp;
+  ifs >> req;
+  
+  BOOST_CHECK_EQUAL(req.method, "POST");
+  BOOST_CHECK_EQUAL(req.url.path, "/test");
+  BOOST_CHECK_EQUAL(req.url.host, "test.org");
+
+  BOOST_CHECK_EQUAL(req.POST()["Hi"], "Moi");
+  BOOST_CHECK_EQUAL(req.POST()["M"], "Bää");
+  BOOST_CHECK_EQUAL(req.POST()["Bai"], "Kai");
+  BOOST_CHECK_EQUAL(req.POST()["Li"], "Ann"); 
+
+  resp = req;
+  resp.status = 200;
+  std::cout << resp << std::endl;
+  std::cout << resp.kind << std::endl;
 }
 
 }

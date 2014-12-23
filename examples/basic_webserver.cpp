94c4b6ccc1742709e548202c475685185fcd2afd
Aki Tuomi <cmouse@desteem.org>
2014-12-23 16:41:06 +0200

    Numerous improvements

--- examples/basic_webserver.cpp
+++ examples/basic_webserver.cpp
@@ -23,11 +23,17 @@ public:
 
   int state;
   int id;
+  time_t last;
 
   ServerRequestResponse() {
     state = 0;
     id = -1;
   };
+
+  void initialize() {
+    req.initialize();
+    resp.initialize();
+  }
 };
 
 /** Really basic simple server */
@@ -60,55 +66,80 @@ public:
     struct sockaddr_in sa;
     socklen_t salen;
     int fd;
+    int val=1;
     char ipaddr[INET_ADDRSTRLEN];
     salen = sizeof(sa);
     memset(&sa,0,sizeof(sa));
     fd = ::accept(lfd, reinterpret_cast<struct sockaddr*>(&sa), &salen);
     if (fd < 0) return; // false alarm
     inet_ntop(AF_INET, &sa.sin_addr, ipaddr, sizeof ipaddr);
+    ::setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+    ::setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &val, sizeof(val));
+    val = 0;
+    ::setsockopt(fd, SOL_SOCKET, SO_LINGER, &val, sizeof(val));
+    pool[fd].initialize();
     pool[fd].id = fd;
     std::cout << "Connection from " << ipaddr << ":" << ntohs(sa.sin_port) << " accepted on fd=" << fd << std::endl;
   }
 
   // handle a request
   void handle(ServerRequestResponse &rr) {
-    if (rr.state == 0) {
-      rr.arl.initialize(&rr.req);
-      rr.state = 1;
-    }
-
-    if (rr.state == 1) {
-      char buf[4096];
-      size_t r;
-      r = ::read(rr.id, buf, sizeof(buf));
-
-      if (r>0) {
-        rr.arl.feed(std::string(buf,r)); 
+    try {
+      if (rr.state == 0) {
+        rr.arl.initialize(&rr.req);
+        rr.state = 1;
       }
-      if (rr.arl.ready()) {
-        rr.arl.finalize();
-        rr.state = 2; 
-      } else return;
+  
+      if (rr.state == 1) {
+        char buf[4096];
+        size_t r;
+        r = ::read(rr.id, buf, sizeof(buf));
+
+        if (r>0) {
+          rr.arl.feed(std::string(buf,r)); 
+        }
+
+        if (rr.arl.ready()) {
+          rr.arl.finalize();
+          rr.state = 2; 
+        } else {
+          if (r == 0) {
+            // EOF
+            ::close(rr.id);
+            rr.state = 0;
+            rr.id = -1;
+          }
+          return;
+        }
+      }
+    } catch (YaHTTP::Error &err) {
+      ::close(rr.id);
+      rr.state = 0;
+      rr.id = -1;
+      std::cout << "YaHTTP::Error: " << err.what() << std::endl;
+      return;
     }
 
     rr.resp.headers["content-type"] = "text/html; charset=utf-8";
+    rr.resp.headers["connection"] = "close";
 
     if (rr.req.url.path == "/") {
-      rr.resp = rr.req;
+      rr.resp.url = rr.req.url;
       rr.resp.status = 200; 
       rr.resp.body = "<!DOCTYPE html>\n<html lang=\"en\"><head><title>Hello, world</title><link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\" /></head><body><h1>200 OK</h1><p>Hello, world</p></body></html>";
     } else if (rr.req.url.path == "/style.css") {
-      rr.resp = rr.req;
+      rr.resp.url = rr.req.url;
       rr.resp.status = 200;
       rr.resp.headers["content-type"] = "text/css; charset=utf-8";
       rr.resp.renderer = YaHTTP::HTTPBase::SendFileRender("style.css");
     } else if (rr.req.url.path == "/bg.jpg") {
-      rr.resp = rr.req;
+      rr.resp.url = rr.req.url;
       rr.resp.status = 200;
       rr.resp.headers["content-type"] = "image/jpeg";
+//      rr.resp.headers["content-length"] = "810816";
       rr.resp.renderer = YaHTTP::HTTPBase::SendFileRender("bg.jpg");
     } else {
-      rr.resp = rr.req;
+      rr.resp.url = rr.req.url;
       rr.resp.status = 404;
       rr.resp.body = "<!DOCTYPE html>\n<html lang=\"en\"><head><title>404 Not Found</title><link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\" /></head><body><h1>404 Not Found</h1><p>Requested URL not found</p></body></html>";
     }

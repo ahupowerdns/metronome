8f53db2007e8f54ab7d99254738b92f2da65c6a0
Fusl <fusl@meo.ws>
2015-08-30 15:04:29 +0200

    Correct code indentations

--- html/engine.js
+++ html/engine.js
@@ -2,12 +2,12 @@
 
 function Metronome(comconfig)
 {
-    if(!(this instanceof Metronome))
-	return new Metronome();
-    this.config=[];
-    this.hierarchy={};
-    this.servers=[];
-    this.comconfig=comconfig;
+	if(!(this instanceof Metronome))
+		return new Metronome();
+	this.config=[];
+	this.hierarchy={};
+	this.servers=[];
+	this.comconfig=comconfig;
 }
 
 /* The ritual:
@@ -52,212 +52,212 @@ The graph config, as passed to setupGraphs:
 // Pass this to 'formula' to get two rates as a percentage of their sum rate
 Metronome.prototype.percentalizer=function(r, d)
 {
-    if(d[0] > 0 && d[1] > 0)
+	if(d[0] > 0 && d[1] > 0)
 	return d[0]*100.0/(d[0] +d[1]);
-    else
+	else
 	return 0;
 }
 
 // Call this to get array of all metrics at a certain level (listMetricaAt('system', 'server1', 'network', 'interfaces'))
 Metronome.prototype.listMetricsAt=function()
 {
-  var ref=this.hierarchy;
-  for(var i = 0; i < arguments.length; ++i) {
-    var tmp = ref[arguments[i]];
-    ref = tmp;
-  }
-  var ret=[];
-  $.each(ref, function(key, val) {
-      ret.push(key);
-    });
-  ret.sort();
-  return ret;
+	var ref=this.hierarchy;
+	for(var i = 0; i < arguments.length; ++i) {
+		var tmp = ref[arguments[i]];
+		ref = tmp;
+	}
+	var ret=[];
+	$.each(ref, function(key, val) {
+		ret.push(key);
+	});
+	ret.sort();
+	return ret;
 }
 
 // the startup function,
 Metronome.prototype.getAllMetrics=function(destination)
 {
-    var qstring = this.comconfig.url+'?do=get-metrics&callback=?&name';
-    var that=this;
-    var alerter = window.setTimeout(function(){ alert('Could not contact Metronome statistics server at '+that.comconfig.url+'. This is either due to a connectivity problem or a intervening firewall, or otherwise a timeout.'); }, 2500);
-    $.getJSON(qstring,
-	      function(data) {
-	          window.clearTimeout(alerter);	
-		  var theservers={};
-		  that.hierarchy={};
-		  $.each(data.metrics, function(a, b) {
-		      var parts = b.split('.');
-		      var name = parts.slice(0,3).join('.');
-		      theservers[name]=1;
+	var qstring = this.comconfig.url+'?do=get-metrics&callback=?&name';
+	var that=this;
+	var alerter = window.setTimeout(function(){ alert('Could not contact Metronome statistics server at '+that.comconfig.url+'. This is either due to a connectivity problem or a intervening firewall, or otherwise a timeout.'); }, 2500);
+	$.getJSON(qstring,
+	function(data) {
+		window.clearTimeout(alerter);	
+		var theservers={};
+		that.hierarchy={};
+		$.each(data.metrics, function(a, b) {
+			var parts = b.split('.');
+			var name = parts.slice(0,3).join('.');
+			theservers[name]=1;
 
-		      for(var i = 0; i < parts.length ; ++i) {
-			  var ref = that.hierarchy;
-			  for(var j = 0 ; j < i; ++j) {
-			      if(ref[parts[j]]==undefined)
-				  ref[parts[j]]={};
-			      ref = ref[parts[j]];
-			  }
-		      }
-		  });
-		  that.servers=[];
-		  $.each(theservers, function(a,b) {
-		      that.servers.push(a);
-		  });
-		  that.servers.sort();
-		  destination(that);
-	      });
+			for(var i = 0; i < parts.length ; ++i) {
+				var ref = that.hierarchy;
+				for(var j = 0 ; j < i; ++j) {
+					if(ref[parts[j]]==undefined)
+					ref[parts[j]]={};
+					ref = ref[parts[j]];
+				}
+			}
+		});
+		that.servers=[];
+		$.each(theservers, function(a,b) {
+			that.servers.push(a);
+		});
+		that.servers.sort();
+		destination(that);
+	});
 }
 	
 Metronome.prototype.updateGraphs=function()
 {
-    var that=this;
-    $.each(this.configs, function(key, val) {
-	that._showGraph(val);
-    });
+	var that=this;
+	$.each(this.configs, function(key, val) {
+		that._showGraph(val);
+	});
 }
 
 Metronome.prototype._showGraph=function(config) {
-    var items = config.items;
+	var items = config.items;
 
-    var qstring = this.comconfig.url+'?do=retrieve&callback=?&name=';
-    var metrics=[];
-    for(var item in items) {
+	var qstring = this.comconfig.url+'?do=retrieve&callback=?&name=';
+	var metrics=[];
+	for(var item in items) {
 	if(items[item].name != undefined)
-	    metrics.push(items[item].name);
-	if(items[item].metrics != undefined) {
-	    $.each(items[item].metrics, function(key, value) {
-		metrics.push(value);
-	    });
+		metrics.push(items[item].name);
+		if(items[item].metrics != undefined) {
+			$.each(items[item].metrics, function(key, value) {
+				metrics.push(value);
+			});
+		}
 	}
-    }
 
-    qstring+= metrics.join(',');
+	qstring+= metrics.join(',');
 
-    var epoch = (new Date).getTime()/1000;
-    qstring+='&begin='+(epoch+this.comconfig.beginTime)+'&end='+(epoch)+'&datapoints='+this.comconfig.datapoints;
+	var epoch = (new Date).getTime()/1000;
+	qstring+='&begin='+(epoch+this.comconfig.beginTime)+'&end='+(epoch)+'&datapoints='+this.comconfig.datapoints;
 
-    var that=this;
-    $.getJSON(qstring,
-	      function(fullseries) {	
-		  var toplot=[];
-		  var grouped={};
+	var that=this;
+	$.getJSON(qstring,
+	function(fullseries) {	
+		var toplot=[];
+		var grouped={};
 		
-		  $.each(metrics, function(num, metric) {
-		      $.each(fullseries.raw[metric], function(key, value) {
-			  if(grouped[value[0]] == undefined) {
-			      grouped[value[0]] = {};
-			      grouped[value[0]].raw = {};
-			      grouped[value[0]].derivative = {};
-			  }
-			  grouped[value[0]].raw[num]=value[1];
-		      });
+		$.each(metrics, function(num, metric) {
+			$.each(fullseries.raw[metric], function(key, value) {
+				if(grouped[value[0]] == undefined) {
+					grouped[value[0]] = {};
+					grouped[value[0]].raw = {};
+					grouped[value[0]].derivative = {};
+				}
+				grouped[value[0]].raw[num]=value[1];
+			});
+
+			$.each(fullseries.derivative[metric], function(key, value) {
+				grouped[value[0]].derivative[num]=value[1];
+			});
+		});
+		//		      console.log('Grouped', grouped);
+		for(var num in items) {
+			var series;
+			if(items[num].kind=='gauge')
+				series = fullseries.raw;
+			else
+				series = fullseries.derivative;
+
+			var factor = 1;
+			if(items[num].bytesToBits != undefined)
+				factor = 8;
+			if(items[num].formula == undefined)
+				toplot[num] = that._coordinateTransform(series[items[num].name], factor);
+
+		}
+
+		for(num in items) {
+			if(items[num].formula != undefined) {
+				toplot[num]=[];
+				$.each(grouped, function(key, value) {
+					toplot[num].push({x: 1.0*key, y: items[num].formula(value.raw, value.derivative) });
+				});
+			}
+		}
+
+		var plotseries=[];
+		var colors=['red', 'steelblue', 'green', 'orange', 'purple', 'black', 'yellow'];
+
+		for(num in items) {
+			plotseries.push( { color: colors[num], data: toplot[num], name: items[num].legend, renderer: 'line'});
+		}
+		config.div.html('<div class='chart_container'><div class='y_axis'></div><div class='chart'></div><div class='legend'></div>');
+
+		var graph = new Rickshaw.Graph( {
+			element: config.div.find('.chart')[0],
+			width: 550,
+			height: 250,
+			renderer: config.renderer || 'multi',
+			padding: { top: 0.05 },
+			series: plotseries
+
+		});
 		
-		      $.each(fullseries.derivative[metric], function(key, value) {
-			  grouped[value[0]].derivative[num]=value[1];
-		      });
-		  });
-		  //		      console.log('Grouped', grouped);
-		  for(var num in items) {
-		      var series;
-		      if(items[num].kind=='gauge')
-			  series = fullseries.raw;
-		      else
-			  series = fullseries.derivative;
-
-		      var factor = 1;
-		      if(items[num].bytesToBits != undefined)
-			  factor = 8;
-		      if(items[num].formula == undefined)
-			  toplot[num] = that._coordinateTransform(series[items[num].name], factor);
+		var axes = new Rickshaw.Graph.Axis.Time( {
+			graph: graph,
+			orientation: 'bottom',
+			timeFixture: new Rickshaw.Fixtures.Time.Local()
+		} );
 		
-		  }
-
-		  for(num in items) {
-		      if(items[num].formula != undefined) {
-			  toplot[num]=[];
-			  $.each(grouped, function(key, value) {
-			      toplot[num].push({x: 1.0*key, y: items[num].formula(value.raw, value.derivative) });
-			  });
-		      }
-		  }
-
-		  var plotseries=[];
-		  var colors=['red', 'steelblue', 'green', 'orange', 'purple', 'black', 'yellow'];
-
-		  for(num in items) {
-		      plotseries.push( { color: colors[num], data: toplot[num], name: items[num].legend, renderer: 'line'});
-		  }
-		  config.div.html('<div class='chart_container'><div class='y_axis'></div><div class='chart'></div><div class='legend'></div>');
-
-		  var graph = new Rickshaw.Graph( {
-		      element: config.div.find('.chart')[0],
-		      width: 550,
-		      height: 250,
-                      renderer: config.renderer || 'multi',
-		      padding: { top: 0.05 },
-		      series: plotseries
-
-		  });
+		var y_ticks = new Rickshaw.Graph.Axis.Y( {
+			graph: graph,
+			orientation: 'left',
+			tickFormat:
+			Rickshaw.Fixtures.Number.formatKMBT,
+			element: config.div.find('.y_axis')[0]
+		} );
 		
-		  var axes = new Rickshaw.Graph.Axis.Time( {
-		      graph: graph,
-		      orientation: 'bottom',
-                      timeFixture: new Rickshaw.Fixtures.Time.Local()
-		  } );
-		
-		  var y_ticks = new Rickshaw.Graph.Axis.Y( {
-		      graph: graph,
-		      orientation: 'left',
-		      tickFormat:
-		      Rickshaw.Fixtures.Number.formatKMBT,
-		      element: config.div.find('.y_axis')[0]
-		  } );
-		
-		  var legend = new Rickshaw.Graph.Legend( {
-                      graph: graph,
-                      element: config.div.find('.legend')[0]
-                  } );		
+		var legend = new Rickshaw.Graph.Legend( {
+			graph: graph,
+			element: config.div.find('.legend')[0]
+		} );		
 	
-		  graph.render();
+		graph.render();
 
-		  var hoverDetail = new Rickshaw.Graph.HoverDetail( {
-		      graph: graph,
+		var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+			graph: graph,
 
-		      formatter: function(series, x, y) {			
-			  var swatch = '<span class='detail_swatch' style='background-color: ' + series.color + ''></span>';
-			  var content = swatch + series.name + ': ' + y.toFixed(2);
-			  return content;
-		      },
-		      xFormatter: function(x) {
-			  return new Date( x * 1000 ).toString();
-		      }
-		  } );
-	      });	
+			formatter: function(series, x, y) {			
+				var swatch = '<span class='detail_swatch' style='background-color: ' + series.color + ''></span>';
+				var content = swatch + series.name + ': ' + y.toFixed(2);
+				return content;
+			},
+			xFormatter: function(x) {
+				return new Date( x * 1000 ).toString();
+			}
+		} );
+	});
 }
 
 // This interval represents a millisecond timestep that will keep the shape of your graph identical if you call updateGraphs
 Metronome.prototype.getNaturalInterval=function()
 {
-    return -this.comconfig.beginTime*1000/this.comconfig.datapoints;
+	return -this.comconfig.beginTime*1000/this.comconfig.datapoints;
 }
 
 
 Metronome.prototype.setupGraphs=function(where, configs)
 {
-  this.configs=[];
-  $(where).html('');
-  for(var a in configs) {
-    configs[a].div = $('<div style="height: 300px;"/>');
-    $(where).append(configs[a].div);
-    this.configs.push(configs[a]);
-  }
+	this.configs=[];
+	$(where).html('');
+	for(var a in configs) {
+		configs[a].div = $('<div style="height: 300px;"/>');
+		$(where).append(configs[a].div);
+		this.configs.push(configs[a]);
+	}
 }
 
 Metronome.prototype._coordinateTransform=function(series, factor)
 {
-    var ret=[];
-    $.each(series, function(a, b) {
-	ret.push({x: b[0], y: factor * b[1]});
-    });
-    return ret;
+	var ret=[];
+	$.each(series, function(a, b) {
+		ret.push({x: b[0], y: factor * b[1]});
+	});
+	return ret;
 }

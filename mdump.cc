dd0e773c5f6500aac55b586df745aa96c732978d
bert hubert <bert.hubert@netherlabs.nl>
2016-06-23 09:14:28 +0200

    add mdump tool to inspect contents of all metrics files

---
+++ mdump.cc
@@ -0,0 +1,34 @@
+#include "statstorage.hh"
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string.hpp>
+
+using std::cin;
+using std::cerr;
+using std::cout;
+using std::endl;
+
+
+// syntax: mmanage
+int main(int argc, char** argv)
+try
+{
+  StatStorage ss("./stats");
+  auto metrics=ss.getMetrics();
+  cout<<"Have "<<metrics.size()<<" metrics"<<endl;
+  for(const auto& m: metrics) {
+    auto vals=ss.retrieve(m);
+    cout<<"Have "<<vals.size()<<" values for "<<m<<endl;
+    for(auto& v : vals)
+      cout<<v.timestamp<<"\t"<<v.value<<endl;
+  }
+
+}
+catch(std::exception& e)
+{
+  cerr<<"Error: "<<e.what()<<endl;
+  exit(EXIT_FAILURE);
+}

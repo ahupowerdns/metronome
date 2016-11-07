3ef98a9aedffcfa78325a9d7ff2ed399fc7f172d
Aki Tuomi <cmouse@cmouse.fi>
2016-11-07 10:44:48 +0200

    Move inline resources to separate files

--- html/local.js
+++ html/local.js
@@ -1,2 +1,7 @@
 "use strict";
 var metronomeServer="http://127.0.0.1:8000/";
+
+$(document).ready(function(){
+  $("#server").on('change', function(){ window.updateFromForm(); }).on('keyup', function(){ window.updateFromForm(); });
+  $("#id").on('change', function(){ window.updateFromForm(); }).on('keyup', function(){ window.updateFromForm(); });
+});
